58ec89c1adc9985d26f945ebf884c7dcfe3d2d3c
arturparkhisenko <ikeagold@gmail.com>
2020-05-17 16:10:38 -0400

    remove signal constant

--- index.js
+++ index.js
@@ -8,8 +8,7 @@
  */
 export const fetchRequest = (input, init) => {
   const controller = new AbortController();
-  const { signal } = controller;
-  const response = fetch(input, { ...init, signal });
+  const response = fetch(input, { ...init, signal: controller.signal});
 
   Object.defineProperty(response, 'cancel', {
     enumerable: true,
